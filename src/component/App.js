cf181b383a795cf4483a2f6a2707d813a9de3cf7
mitrafantos <mitrafantos@gmail.com>
2021-01-22 12:19:08 +0300

    changed firebase config

--- src/component/App.js
+++ src/component/App.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-const firebaseConfig = process.env.REACT_APP_FIREBASE_CONFIG;
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
