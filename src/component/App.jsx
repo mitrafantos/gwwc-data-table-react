4fdf5c22adcc9a89f0a0602e7d7372b9dd5101cc
mitrafantos <mitrafantos@gmail.com>
2021-01-23 21:49:11 +0300

    switched to jsx

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -5,7 +5,8 @@ import { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+const firebaseConfig = JSON.parse
+(process.env.REACT_APP_FIREBASE_CONFIG);
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
