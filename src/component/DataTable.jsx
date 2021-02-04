2a89cc6df5bcb85aab237bd1169bab5aa72f6606
mitrafantos <mitrafantos@gmail.com>
2021-02-04 12:22:30 +0300

    adjusted data processing scripts after google script changes

--- src/component/DataTable.jsx
+++ src/component/DataTable.jsx
@@ -6,15 +6,13 @@ import PropTypes from 'prop-types';
 import { DataGrid } from '@material-ui/data-grid';
 
 function getColumns(headers) {
-  return headers
-    .filter((el) => el !== null)
-    .map((header) => ({ flex: true, width: 200, ...header }));
+  return headers;
 }
 
 function getRows(organisations) {
   return Object.entries(organisations)
     .filter(([slug]) => slug !== 'headers')
-    .map(([slug, row]) => ({ id: slug, ...row }));
+    .map(([, row]) => row);
 }
 
 export default function DataTable(props) {
